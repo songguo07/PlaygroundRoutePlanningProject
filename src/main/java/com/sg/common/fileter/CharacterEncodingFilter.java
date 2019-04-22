f525629031daa26ca2d2c376104d581855a0c8bc
lyxwz <1767757232@qq.com>
2019-04-22 20:46:10 +0800

    用户不登陆无法进入系统过滤器

--- src/main/java/com/sg/common/fileter/CharacterEncodingFilter.java
+++ src/main/java/com/sg/common/fileter/CharacterEncodingFilter.java
@@ -8,6 +8,8 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /*
  * 设置所有页面的编码方式
@@ -21,15 +23,16 @@ public class CharacterEncodingFilter implements Filter{
 	}
 
 	@Override
-	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain arg2)
 			throws IOException, ServletException {
-		arg0.setCharacterEncoding(encoding);//硬编码
-		arg2.doFilter(arg0, arg1);
+		request.setCharacterEncoding(encoding);//硬编码
+		arg2.doFilter(request, response);
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
 		String encoding = arg0.getInitParameter("encoding");
+		System.out.println("1********************过滤器");
 		if(encoding != null && !"".equals(encoding)) {
 			this.encoding = encoding;
 		}
---
+++ src/main/java/com/sg/common/fileter/ClearUserHobbyFilter.java
@@ -0,0 +1,82 @@
+package com.sg.common.fileter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.sg.forestage.user.entity.User;
+
+
+@WebFilter(filterName="/ClearUserHobbyFilter",urlPatterns="/*")
+public class ClearUserHobbyFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public ClearUserHobbyFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = request.getSession();
+		
+		User user = (User) session.getAttribute("userSession");
+		System.out.println(user);
+		
+		if(user==null) {
+			
+		}
+        String uri = request.getRequestURI(); 
+        System.out.println("uri---->" + uri);
+		  if(uri.contains(".gif") || uri.contains(".css") || uri.contains(".js") || uri.contains(".png")|| uri.contains(".jpg")|| 
+	        		uri.equals(((HttpServletRequest) request).getContextPath() + "/views/login_regist/login.jsp'")||
+	        		uri.equals(((HttpServletRequest) request).getContextPath() + "/views/login_regist/regist.jsp'")||
+	        		uri.equals(((HttpServletRequest) request).getContextPath() + "/views/login_regist/forget_password.jsp'")){
+	            //如果发现是css或者js文件，直接放行
+	        	System.out.println("直接放行");
+	        	chain.doFilter(request, response);
+	        }else{
+				if(session!=null&&session.getAttribute("userSession")==null) {
+					System.out.println("agaagggggggggggggggggg");
+					 response.setContentType("text/html;charset=utf-8");
+		             PrintWriter out = response.getWriter();
+		             out.println("<script language='javascript' type='text/javascript'>");
+		             out.println("window.top.location.href='" + ((HttpServletRequest) request).getContextPath() + "/views/login_regist/login.jsp'");
+		             out.println("</script>");
+				}else {
+					chain.doFilter(request, response);
+				}
+			}
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		System.out.println("2********************过滤器");
+	}
+
+}
